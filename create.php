ca25d77b13043e08e493970350a41f05a3e1e781
Lyana Rayna <yyanaarayyna@gmail.com>
2025-12-26 14:42:14 +0800

    Add UI for create and delete page

--- create.php
+++ create.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
+    <title>Create ToDo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do List Assistant</title>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+
+    <h2>Tambah ToDo</h2>
+
+    <form>
+        <label>Judul</label><br>
+        <input type="text" placeholder="Masukkan judul ToDo"><br><br>
+
+        <label>Deskripsi</label><br>
+        <textarea rows="4" placeholder="Masukkan deskripsi ToDo"></textarea><br><br>
+
+        <button>Simpan</button>
+    </form>
+
+    <br>
+    <a href="index.php">← Kembali</a>
+
 </body>
 </html>
